80b34aac370bb717f60ee8f2709dbf1b4a5bf9b4
LightningCorvidae <106744894+LightningCorvidae@users.noreply.github.com>
2022-06-29 15:57:54 +0100

    Updated Files
    Created Queue class and set variables

--- squireQueue.py
+++ squireQueue.py
@@ -1,27 +1,40 @@
-#Queue section of the squire bot.
+#Class Definition for Queues
+class Queue:
+    teamSize = 3
+    numberOfTeams = 3
+    queueSize = teamSize * numberOfTeams
+    teamNumber = 1
 
-#Variables
+    def _init(self, teamSize, numberOfTeams):
+        self.teamSize = teamSize
+        self.numberOfTeams = numberofTeams
 
-teamSize = 3
-numberOfTeams = 3
-
-def showQueue():
-    when chat !list
-    for number of teams
+    
+    def showQueue():
+        when chat !list
+        for number of teams
         team x : usernames
 
-def joinQueue():
-    when chat !join
-    get username that put !join
-    add to team
+    def joinQueue():
+        when chat !join
+        get username that put !join
+        add to team
 
-def leaveQueue():
-    when chat !leave
-    get username that put !leave
-    remove from queue
+    def leaveQueue():
+        when chat !leave
+        get username that put !leave
+        remove from queue
 
-def addToTeam():
-    if team is not full
-    add username to team
-    else
-    check next team number
+
+
+    def addToTeam():
+        if team is not full
+        add username to team
+        else
+        check next team number
+
+    def createTeam():
+        
+      
+
+
